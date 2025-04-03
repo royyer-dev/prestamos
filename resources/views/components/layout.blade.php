b74e8e9f3fa2309d834134e0289e28116b090e92
Rogelio Corona <coronaroy02@gmail.com>
2025-04-03 15:30:34 -0600

    secure asset

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <!-- importar las librerías de bootstrap -->
-    <link rel="stylesheet" href={{ URL::asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }} />
+    <link rel="stylesheet" href={{ secure_asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }} />
     <!-- importar los archivos JavaScript de Bootstrap-->
-    <script src={{ URL::asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}></script>
+    <script src={{ secure_asset('bootstrap-5.3.3-dist/js/bootstrap.min.js') }}></script>
     <!-- importar librerías de estilos y javascript de datatables para manipular tablas desde el
     navegador del usuario-->
-    <link href={{ URL::asset('DataTables/datatables.min.css')}} rel="stylesheet"/>
-    <script src={{ URL::asset('DataTables/datatables.min.js')}}></script>
-    <link href={{URL::asset("assets/style.css")}} rel="stylesheet" />
+    <link href={{ secure_asset('DataTables/datatables.min.css')}} rel="stylesheet"/>
+    <script src={{ secure_asset('DataTables/datatables.min.js')}}></script>
+    <link href={{ secure_asset("assets/style.css")}} rel="stylesheet" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Préstamos Coliman</title>
